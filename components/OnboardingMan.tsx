7dbb8d8df4e3554210908075b667811f27b64210
redizogolli <redi.zogolli@gmail.com>
2025-05-19 12:18:40 +0200

    onboarding shape fix

--- components/OnboardingMan.tsx
+++ components/OnboardingMan.tsx
@@ -1,21 +1,30 @@
-import { View, Image } from 'react-native'
-import React from 'react'
+import { View, Image } from "react-native";
 
 const OnboardingMan = () => {
   return (
-    <View className='flex-1 w-full bg-light-primary'>
-      {/* <Image source={require('../assets/images/Man.png')}/> */}
-      <View className='flex-1 items-center justify-end relative'>
+    <View className="flex-1 w-full relative bg-white">
+      <View className="flex-1 items-center justify-end bg-light-primary">
         {/* Images */}
-        <View className='relative'>
-          <Image source={require('../assets/images/Man.png')} className='w-man' />
-          <Image source={require('../assets/images/Coin.png')} className='z-10 absolute left-[-20px]' />
-          <Image source={require('../assets/images/Donut.png')} className='z-10 absolute right-[-20px] top-16' />
-        </View>
-        <Image source={require('../assets/images/Shadow.png')} className='w-full' />
+        <Image
+          source={require("../assets/images/Man.png")}
+          className="z-10 w-man absolute bottom-2"
+        />
+        <Image
+          source={require("../assets/images/Coin.png")}
+          className="z-10 absolute left-14 top-36"
+        />
+        <Image
+          source={require("../assets/images/Donut.png")}
+          className="z-10 absolute right-14 top-52"
+        />
+        <Image
+          source={require("../assets/images/Shadow.png")}
+          className="w-full z-10 absolute bottom-[-20px]"
+        />
+        <View className="bg-transparent w-full h- border-b-[50px] border-r-[450px] border-r-transparent border-b-white"></View>
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default OnboardingMan
+export default OnboardingMan;
