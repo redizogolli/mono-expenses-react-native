4a0a4ad2da4603decae77ea591c8ffb56488e354
redizogolli <redi.zogolli@gmail.com>
2025-05-23 16:34:31 +0200

    scrollbar-home

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,12 +1,97 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, ScrollView, FlatList, Dimensions } from "react-native";
 import React from "react";
 import HeaderBackground from "@/components/HeaderBackground";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import BalanceInfo from "@/components/BalanceInfo";
 import TransactionItem from "@/components/TransactionItem";
+import { FlashList } from "@shopify/flash-list";
 
 const Home = () => {
+  const transactions = [
+    {
+      title: "Upwork",
+      date: new Date(),
+      amount: 850,
+    },
+    {
+      title: "Transfer",
+      date: new Date(),
+      amount: -85,
+    },
+    {
+      title: "Paypal",
+      date: new Date(),
+      amount: 150,
+    },
+    {
+      title: "Transfer",
+      date: new Date(),
+      amount: -100,
+    },
+    {
+      title: "Youtube",
+      date: new Date(),
+      amount: 385,
+    },
+    {
+      title: "Transfer",
+      date: new Date(),
+      amount: -50,
+    },
+    {
+      title: "Transfer",
+      date: new Date(),
+      amount: -100,
+    },
+    {
+      title: "Transfer",
+      date: new Date(),
+      amount: -100,
+    },
+    {
+      title: "Transfer",
+      date: new Date(),
+      amount: -85,
+    },
+    {
+      title: "Paypal",
+      date: new Date(),
+      amount: 150,
+    },
+    {
+      title: "Transfer",
+      date: new Date(),
+      amount: -100,
+    },
+    {
+      title: "Transfer",
+      date: new Date(),
+      amount: -100,
+    },
+    {
+      title: "Transfer",
+      date: new Date(),
+      amount: -100,
+    },
+    {
+      title: "Transfer",
+      date: new Date(),
+      amount: -85,
+    },
+    {
+      title: "Paypal",
+      date: new Date(),
+      amount: 150,
+    },
+    {
+      title: "Transfer",
+      date: new Date(),
+      amount: -100,
+    },
+  ];
+
   return (
+    <ScrollView showsVerticalScrollIndicator={false}>
     <View className="flex-1 bg-primary-white">
       <HeaderBackground>
         <View className="flex-1 flex-row justify-between px-4 pt-16">
@@ -34,18 +119,40 @@ const Home = () => {
       </HeaderBackground>
       <View className="w-full px-5 -mt-32 h-52">
         <BalanceInfo />
-        <View className="mt-8 ">
+      </View>
+      <View className="flex-1 mt-8 px-5">
           <View className="flex-row justify-between items-center">
-            <Text className="text-lighter-black font-semibold text-lg">Transactions History</Text>
+            <Text className="text-lighter-black font-semibold text-lg">
+              Transactions History
+            </Text>
             <Text className="text-slate-500 text-sm">See all</Text>
           </View>
-          <View className="flex mt-5 gap-4">
-            <TransactionItem  title="Upwork" date={new Date()} amount={850}/>
-            <TransactionItem  title="Transfer" date={new Date()} amount={-85}/>
+          <View className="flex-1 mt-5 flex gap-4">
+            {/* <FlashList
+              estimatedItemSize={41}
+              data={transactions}
+              keyExtractor={(_: any, index: number) => index.toString()}
+              renderItem={({item,index} : any) => (
+                <TransactionItem
+                key={index}
+                title={item.title}
+                date={item.date}
+                amount={item.amount}
+              />
+              )}
+            /> */}
+            {transactions.map((item, index) => (
+              <TransactionItem
+              key={index}
+              title={item.title}
+              date={item.date}
+              amount={item.amount}
+            />
+            ))}
           </View>
         </View>
-      </View>
     </View>
+    </ScrollView>
   );
 };
 
